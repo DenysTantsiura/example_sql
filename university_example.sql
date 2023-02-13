eae9e86441f583ef1b6f50d06aa170fd32f1b2ff
Denys Tantsiura <ghost0002501@gmail.com>
2023-02-13 23:51:40 +0200

    Fixed. Creating and filling is Ok.

--- university_example.sql
+++ university_example.sql
@@ -1,40 +1,40 @@
 /*The example shows various uses 
-of some similar commands (sequence)*/
+    of some similar commands (sequence)*/
 
 -- Table: students
 DROP TABLE IF EXISTS students;
 CREATE TABLE students (
-  id INT UNSIGNED PRIMARY KEY AUTOINCREMENT,
-  name VARCHAR(50) NOT NULL,
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(50) UNIQUE NOT NULL,
   group_id INT UNSIGNED,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT students_name_un UNIQUE KEY (name),
+  -- CONSTRAINT students_name_un UNIQUE KEY (name),
   FOREIGN KEY (group_id) REFERENCES groups (id)
         ON DELETE SET NULL
         ON UPDATE CASCADE
-);		
+);
 			
 -- Table: groups
 DROP TABLE IF EXISTS groups;
 CREATE TABLE groups (
-  id INT UNSIGNED PRIMARY KEY AUTOINCREMENT,
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
   group_name CHAR(10) UNIQUE NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);		
+);
 		
 -- Table: teachers
 DROP TABLE IF EXISTS teachers;
 CREATE TABLE teachers (
-  id INT UNSIGNED PRIMARY KEY AUTOINCREMENT,
-  name VARCHAR(50) NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT teachers_name_uq UNIQUE KEY (name)
-);			
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(50) UNIQUE NOT NULL,
+  -- CONSTRAINT teachers_name_uq UNIQUE KEY (name),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 			
 -- Table: subjects
 DROP TABLE IF EXISTS subjects;
 CREATE TABLE subjects (
-  id INT UNSIGNED PRIMARY KEY AUTOINCREMENT,
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
   subject CHAR(30), 
   teacher_id INT UNSIGNED,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -46,7 +46,7 @@ CREATE TABLE subjects (
 -- Table: assessments
 DROP TABLE IF EXISTS assessments;
 CREATE TABLE assessments (
-  id INT UNSIGNED PRIMARY KEY AUTOINCREMENT, 
+  id INTEGER PRIMARY KEY AUTOINCREMENT, 
   value_ TINYINT UNSIGNED NOT NULL, 
   date_of DATE NOT NULL,
   subject_id INT UNSIGNED, 
