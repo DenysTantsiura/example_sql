c956aeba1bd2e5af32d82fc5b0eb5547bc5465f7
Denys Tantsiura <ghost0002501@gmail.com>
2023-02-17 17:22:17 +0200

    In postgresql 12 query bag? =(.

--- sql_requests_postgresql/query_12.sql
+++ sql_requests_postgresql/query_12.sql
@@ -6,7 +6,7 @@ SELECT
   s.name AS Student,
   g.group_name AS Group_,
   sub.subject AS Subject,
-  DATE(a.date_of) AS "Date of" -- --CONVERT(varchar, a.date_of,2) AS [YYYY-MM.DD]
+  DATE(a.date_of) AS Date_of -- --CONVERT(varchar, a.date_of,2) AS [YYYY-MM.DD]
 FROM
   assessments AS a
   JOIN
@@ -19,6 +19,6 @@ FROM
   groups_ AS g
 	  ON s.group_id = g.id
 WHERE g.id = 3 AND sub.id = 2 -- Group_ = "Group-2" AND Subject = "History" --AND a.date_of = MAX(a.date_of)
-GROUP BY Student, Group_, Subject, a.date_of, Assessment
-ORDER BY Student, a.date_of DESC;
+GROUP BY Student, Group_, Subject, Date_of, Assessment
+ORDER BY Student, Date_of DESC;
 
--- sql_requests_postgresql/query_2.sql
+++ sql_requests_postgresql/query_2.sql
@@ -22,7 +22,7 @@ ORDER BY
 	AVG(a.value_) DESC, 
 	a.subject_id LIMIT 1;
 
--- alternative...:
+-- alternative not working...:
 /*SELECT 
     first_.id, 
     first_.Student, 
